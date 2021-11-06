16f75751dae854fc92450f6314b735c406432eb7
Rafael D'Santi <dsantirafael@gmail.com>
2021-11-05 21:47:09 -0300

    mudança de porta

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const HLTV = require('../node_modules/hltv-api/dist/index.js').default
+const HLTV = require('dist/index.js').default
 const makeEndpoints = require('endpoints.js')
 
 const app = express()
 
 makeEndpoints(app, HLTV)
 
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Listening csgo-api on port http://localhost:${PORT} ...`)
